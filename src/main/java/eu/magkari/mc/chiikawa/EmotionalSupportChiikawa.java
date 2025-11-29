d53bfa6375efc43e6dcfba1772d9ee09c822f188
Magnus <magkari@pm.me>
2025-11-29 20:34:22 +0000

    update

--- src/main/java/eu/magkari/mc/chiikawa/EmotionalSupportChiikawa.java
+++ src/main/java/eu/magkari/mc/chiikawa/EmotionalSupportChiikawa.java
@@ -1,30 +1,27 @@
 package eu.magkari.mc.chiikawa;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gl.RenderPipelines;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.resources.ResourceLocation;
 
 public class EmotionalSupportChiikawa implements ClientModInitializer {
 	public static final String MOD_ID = "chiikawa";
 
-	public static final Identifier CHIIKAWA = Identifier.of(MOD_ID, "hud/chiikawa");
+	public static final ResourceLocation CHIIKAWA = ResourceLocation.fromNamespaceAndPath(MOD_ID, "hud/chiikawa");
 
 	@Override
 	public void onInitializeClient() {
-		HudElementRegistry.attachElementAfter(VanillaHudElements.CHAT, Identifier.of(MOD_ID, "chiikawa_hud"), EmotionalSupportChiikawa::render);
+		HudElementRegistry.attachElementAfter(VanillaHudElements.CHAT, ResourceLocation.fromNamespaceAndPath(MOD_ID, "chiikawa_hud"), EmotionalSupportChiikawa::render);
 	}
 
-	private static void render(DrawContext context, RenderTickCounter tickCounter) {
-		int windowWidth = MinecraftClient.getInstance().getWindow().getWidth();
-		context.drawGuiTexture(RenderPipelines.GUI_TEXTURED, CHIIKAWA, windowWidth/150, windowWidth/150, windowWidth/20, windowWidth/20);
+	private static void render(GuiGraphics context, DeltaTracker deltaTracker) {
+		int windowWidth = Minecraft.getInstance().getWindow().getScreenWidth();
+		context.blitSprite(RenderPipelines.GUI_TEXTURED, CHIIKAWA, windowWidth/150, windowWidth/150, windowWidth/20, windowWidth/20);
 	}
 }
